bc5421082602d80361b15d8ae8fa7f83c3482172
Joe Wingbermuehle <joewing@joewing.net>
2013-09-29 14:24:32 -0500

    More DRAM unit tests.

--- Tests/TestDRAM.py
+++ Tests/TestDRAM.py
@@ -22,7 +22,64 @@ class TestDRAM(unittest.TestCase):
                   open_page_mode = True)
       dram.reset(self.machine)
 
-      dram.process(False, 0, 4)
-      self.assertEqual(self.machine.time, 22)
+      dram.process(False, 0, 4) # Miss
+      t = 22
+      self.assertEqual(self.machine.time, t)
 
+      dram.process(False, 4, 4)  # Hit
+      t += 8
+      self.assertEqual(self.machine.time, t)
+
+      dram.process(False, 2097152, 4)  # Miss
+      t += 22
+      self.assertEqual(self.machine.time, t)
+
+      dram.process(True, 2097152, 8)   # Hit
+      t += 16
+      self.assertEqual(self.machine.time, t)
+
+      dram.process(True, 2097152 - 4, 8)  # Miss/hit
+      t += 30
+      self.assertEqual(self.machine.time, t)
+
+      dram.process(False, 4, 4)  # Miss/write-back
+      t += 24
+      self.assertEqual(self.machine.time, t)
+
+   def test_closed(self):
+      dram = DRAM(multiplier = 2,
+                  cas_cycles = 2,
+                  rcd_cycles = 3,
+                  rp_cycles  = 4,
+                  wb_cycles  = 1,
+                  page_size  = 1024,
+                  page_count = 2048,
+                  width      = 2,
+                  burst_size = 2,
+                  open_page_mode = False)
+      dram.reset(self.machine)
+
+      dram.process(False, 0, 4)
+      t = 14
+      self.assertEqual(self.machine.time, t)
+
+      dram.process(False, 4, 4)
+      t += 22
+      self.assertEqual(self.machine.time, t)
+
+      dram.process(False, 2097152, 4)
+      t += 14
+      self.assertEqual(self.machine.time, t)
+
+      dram.process(True, 2097152, 8)
+      t += 46
+      self.assertEqual(self.machine.time, t)
+
+      dram.process(True, 2097152 - 4, 8)
+      t += 28
+      self.assertEqual(self.machine.time, t)
+
+      dram.process(False, 4, 4)
+      t += 14
+      self.assertEqual(self.machine.time, t)
 
