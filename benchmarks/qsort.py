81daed1e29c63f857412f0d8ec68f045cb954471
Joe Wingbermuehle <joewing@joewing.net>
2013-10-16 10:39:49 -0500

    Attempt to fix a memory leak.

--- benchmarks/qsort.py
+++ benchmarks/qsort.py
@@ -12,40 +12,40 @@ class QSort(base.Benchmark):
       self.size = size
       self.input_port = input_port
       self.output_port = output_port
+      self.array = [0] * self.size
 
-   def _sort(self, array, left, right):
+   def _sort(self, left, right):
       yield self.read(left)
-      pivot = array[left]
+      pivot = self.array[left]
       a, b = left, right
       while True:
          while a <= right:
             yield self.read(a)
-            if array[a] >= pivot: break
+            if self.array[a] >= pivot: break
             a += 1
          while b >= left:
             yield self.read(b)
-            if array[b] <= pivot: break
+            if self.array[b] <= pivot: break
             b -= 1
          if a > b: break
-         array[a], array[b] = array[b], array[a]
+         self.array[a], self.array[b] = self.array[b], self.array[a]
          yield self.write(a)
          yield self.write(b)
          a += 1
          b -= 1
       if a - 1 > left:
-         for t in self._sort(array, left, a - 1):
+         for t in self._sort(left, a - 1):
             yield t
       if right > a:
-         for t in self._sort(array, a, right):
+         for t in self._sort(a, right):
             yield t
 
    def run(self):
       rand = random.Random(self.seed)
-      array = []
       for i in range(self.size):
-         array.append(rand.randint(0, 1 << 30))
+         self.array[i] = rand.randint(0, 1 << 30)
          yield self.consume(self.input_port)
-      for a in self._sort(array, 0, self.size - 1):
+      for a in self._sort(0, self.size - 1):
          yield a
       for i in range(self.size):
          yield self.produce(self.output_port)
