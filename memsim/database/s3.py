445b31cb35efcd704bbda7a2129f82c8afab457b
Joe Wingbermuehle <joewing@joewing.net>
2013-11-11 14:30:21 -0600

    Update S3 database connector.

--- memsim/database/s3.py
+++ memsim/database/s3.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 import json
 import os
 
-from boto.utils import get_instance_metadata
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
@@ -12,69 +11,101 @@ from memsim.database import base
 
 class S3Database(base.Database):
 
-    def __init__(self, m):
+    def __init__(self, m=''):
         """Initialize."""
-        self.data = dict()
-        self.name = None
+        base.Database.__init__(self, m)
+        self.model_hash = self.get_hash(self.model)
+        self.state = dict()
+        self.results = dict()
+
+    def load(self):
+        """Load the current model state from the database."""
+        akey = os.environ['AWS_ACCESS_KEY']
+        skey = os.environ['AWS_SECRET_KEY']
+        bucket_name = os.environ['AWS_S3_BUCKET']
+
+        # Attempt to connect to the database.
+        print("Trying ", bucket_name)
         try:
-            akey = os.environ['AWS_ACCESS_KEY']
-            skey = os.environ['AWS_SECRET_KEY']
             self.conn = S3Connection(akey, skey)
-            self.bucket = self.conn.get_bucket(os.environ['AWS_S3_BUCKET'])
-        except Exception as e:
-            print("ERROR: could not connect to the database:", e)
-
-    def list(self):
-        """Get a list of named states."""
-        for n in self.bucket.list():
-            yield n
-
-    def load(self, name):
-        """Load the specified named state."""
-        self.name = name
-        try:
-            k = Key(self.bucket)
-            k.key = name
-            self.data = json.loads(k.get_contents_as_string())
-        except Exception as e:
-            print("state not loaded:", e)
-
-    def set_instance(self):
-        if 'instance' in self.data:
+            self.bucket = self.conn.get_bucket(bucket_name)
+        except:
             return False
-        else:
-            meta = get_instance_metadata()
-            instance = meta['instance-id']
-            self.set_value('instance', instance)
-            return True
 
-    def clear_instance(self):
-        if 'instance' in self.data:
-            del self.data['instance']
-
-    def get_instance(self):
-        return self.get_value('instance')
+        # Attempt to read the current state.
+        # If this fails, it means that we are to create a new state.
+        try:
+            key = '-'.join(['state', self.model_hash])
+            k = Key(self.bucket, name=key)
+            self.state = json.loads(k.get_contents_as_string())
+        except:
+            pass
+        return True
 
     def save(self):
-        """Save state."""
-        if self.name is None:
-            print("ERROR: no state loaded")
-            return
-        try:
-            k = Key(self.bucket)
-            k.key = self.name
-            k.set_contents_from_string(json.dumps(self.data))
-        except Exception as e:
-            print("ERROR: could not save state: ", e)
+        """Save the current model state to the database."""
+        key = '-'.join(['state', self.model_hash])
+        k = Key(self.bucket, name=key)
+        k.set_contents_from_string(json.dumps(self.state))
 
-    def has_value(self, key):
-        """Determine if the specified key exists."""
-        return key in self.data
+    def _get_key(self, prefix, name):
+        """Get the key name for an entry in the database."""
+        return '-'.join([prefix, self.model_hash, self.get_hash(name)])
 
-    def get_value(self, key):
-        """Get the value associated with a key."""
-        return self.data.get(key)
+    def _get(self, prefix, name):
+        """Load a value from the database."""
+        key = self._get_key(prefix, name)
+        if key in self.results:
+            return self.results[key]
+        k = self.bucket.get_key(key)
+        return json.loads(k.get_contents_as_string()) if k else None
 
-    def set_value(self, key, value):
-        """Set the value associated with a key."""
-        self.data[key] = value
+    def _put(self, prefix, name, value):
+        """Store a value to the database."""
+        key = self._get_keY(prefix, name)
+        self.results[key] = value
+        k = Key(self.bucket, name=key)
+        k.set_contents_from_string(json.dumps(value))
+
+    def get_result(self, mem):
+        """Get a result from the database."""
+        return self._get('result', mem)
+
+    def add_result(self, mem, value):
+        """Insert a result to the database."""
+        self._put('result', mem, value)
+
+    def get_fpga_result(self, name):
+        """Get FPGA timing data from the database."""
+        return self._get('fpga', name)
+
+    def add_fpga_result(self, name, frequency, bram_count):
+        """Add an FPGA timing result to the database."""
+        self._put('fpga', name, (frequency, bram_count))
+
+    def get_cacti_result(self, name):
+        """Get CACTI timing data from the database."""
+        return self._get('cacti', name)
+
+    def add_cacti_result(self, name, access_time, cycle_time, area):
+        """Add CACTI timing data to the database."""
+        value = (access_time, cycle_time, area)
+        self._put('cacti', name, value)
+
+    def get_states(self):
+        """Generator to return all persisted states."""
+        for k in self.bucket.list(prefix='state-'):
+            yield json.loads(k.get_contents_as_string())
+
+    def remove(self, h):
+        """Remove data for the specified model hash."""
+
+        # Remove results.
+        result_prefix = '-'.join(['result', self.model_hash, ''])
+        for k in self.bucket.list(prefix=result_prefix):
+            k.delete()
+
+        # Remove the state.
+        state_key = '-'.join(['state', self.model_hash])
+        k = Key(self.bucket, name=state_key)
+        k.delete()
