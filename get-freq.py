df381a6934d8414ee407548f20b6564673be8af1
Joe Wingbermuehle <joewing@joewing.net>
2013-11-13 11:58:25 -0600

    Fix get-freq.py.

--- get-freq.py
+++ get-freq.py
@@ -3,9 +3,9 @@ from __future__ import print_function
 import optparse
 import sys
 
-import lex
-import model
-import memory.xilinx as xilinx
+from memsim import model
+from memsim.memory import xilinx
+
 
 parser = optparse.OptionParser()
 parser.add_option('-m', '--model', dest='model', default=None,
@@ -16,16 +16,19 @@ parser.add_option('-k', '--keep', dest='keep', default=False,
 
 
 def main():
-    (options, args) = parser.parse_args()
+    options, args = parser.parse_args()
     if options.model is None:
-        print("ERROR: no model specified")
+        print('ERROR: no model specified')
         sys.exit(-1)
-    with open(options.model) as f:
-        m = model.parse_model(lex.Lexer(f))
-        m.machine.frequency = 1 << 31
-        result = xilinx.run_xilinx(m.machine, m.memory, options.keep)
-        print("Frequency:", result.frequency)
-        print("BRAMs:     ", result.bram_count)
+    m = model.parse_model_file(options.model)
+    if not m:
+        print('ERROR: could not read model')
+        sys.exit(-1)
+    m.machine.frequency = 1 << 31
+    result = xilinx.run_xilinx(m.machine, m.memory, options.keep)
+    print("Frequency:", result.frequency)
+    print("BRAMs:     ", result.bram_count)
+
 
 if __name__ == '__main__':
     main()
