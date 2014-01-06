8b256e98f69ca33517e30b451bccea96d8fd46f7
Joe Wingbermuehle <joewing@joewing.net>
2014-01-06 11:27:27 -0600

    Fix memgen.py.

--- memgen.py
+++ memgen.py
@@ -1,24 +1,21 @@
 
+from __future__ import print_function
 import optparse
 import sys
 
 from memsim import model, vhdl
 
 
-parser = optparse.OptionParser()
-parser.add_option('-m', '--model', dest='model', default=None,
-                  help='file containing the memory subsystem specification')
-
-
 def main():
+    parser = optparse.OptionParser()
     options, args = parser.parse_args()
-    if not options.model:
+    if len(args) < 1:
         print('ERROR: no model file specified')
         sys.exit(-1)
-    m = model.parse_model_file(options.model)
-    if not m:
-        print('ERROR: could not read model')
+    elif len(args) > 1:
+        print('ERROR: too many options')
         sys.exit(-1)
+    m = model.parse_model_file(args[0])
     gen = vhdl.VHDLGenerator()
     result = gen.generate(m.machine, m.memory)
     print(result)
