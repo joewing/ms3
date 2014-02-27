dd2b5395a7715557efb1b96e2bbda0fb49c34fe7
Joe Wingbermuehle <joewing@joewing.net>
2014-02-27 10:37:51 -0600

    Fix HDL tests.

--- hdl/run-tests.sh
+++ hdl/run-tests.sh
@@ -2,18 +2,22 @@
 
 MAIN_MEM="(ram (latency 10)(burst 0))"
 
+set -e
+
 function run()
 {
-    echo "(machine"                     >  input.model
-    echo "  (word_size 4)"              >> input.model
-    echo "  (addr_bits 32)"             >> input.model
-    echo "  (target fpga)"              >> input.model
-    echo "  (frequency 250000000)"      >> input.model
-    echo "  (max_cost 64)"              >> input.model
-    echo ")"                            >> input.model
-    echo "(memory"                      >> input.model
-    cat input.mem                       >> input.model
-    echo ")"                            >> input.model
+    echo "(machine"                         >  input.model
+    echo "  (addr_bits 32)"                 >> input.model
+    echo "  (target fpga)"                  >> input.model
+    echo "  (frequency 250000000)"          >> input.model
+    echo "  (max_cost 64)"                  >> input.model
+    echo ")"                                >> input.model
+    echo "(memory"                          >> input.model
+    echo "(main (memory $MAIN_MEM))"        >> input.model
+    echo "(subsystem (id 0)(word_size 4)"   >> input.model
+    echo "(memory"                          >> input.model
+    cat input.mem                           >> input.model
+    echo ")))"                              >> input.model
     cat input.mem
     pypy ../memgen.py input.model > mem.vhdl 2> /dev/null
     ghdl -a mem.vhdl tb.vhdl
@@ -26,7 +30,7 @@ function run()
     rm input.mem input.model mem.vhdl
 }
 
-ghdl -a ram.vhdl
+ghdl -a ram.vhdl adapter.vhdl
 
 echo "Testing cache..."
 ghdl -a cache.vhdl
@@ -40,7 +44,7 @@ for ((lsize=8; lsize<=32; lsize=lsize*2)) ; do
                     echo -n "(associativity $a)"        >> input.mem
                     echo -n "(policy $p)"               >> input.mem
                     echo -n "(write_back $wb)"          >> input.mem
-                    echo "(memory $MAIN_MEM))"          >> input.mem
+                    echo "(memory (main)))"             >> input.mem
                     run
                 done
             done
@@ -51,7 +55,7 @@ done
 echo "Testing spm..."
 ghdl -a spm.vhdl
 for ((size=8; size<=256; size=size*2)) ; do
-    echo "(spm (size $size) (memory $MAIN_MEM))" > input.mem
+    echo "(spm (size $size) (memory (main)))" > input.mem
     run
 done
 
@@ -59,16 +63,16 @@ echo "Testing split..."
 ghdl -a split.vhdl
 echo -n "(split (offset 24)"            >  input.mem
 echo -n "(bank0 (join))(bank1 (join))"  >> input.mem
-echo    "(memory $MAIN_MEM))"           >> input.mem
+echo    "(memory (main)))"              >> input.mem
 run
 
 echo "Testing offset..."
 ghdl -a offset.vhdl
-echo "(offset (value 8)(bank (join))(memory $MAIN_MEM))" > input.mem
+echo "(offset (value 8)(bank (join))(memory (main)))" > input.mem
 run
-echo "(offset (value -16)(bank (join))(memory $MAIN_MEM))" > input.mem
+echo "(offset (value -16)(bank (join))(memory (main)))" > input.mem
 run
-echo "(offset (value 3)(bank (join))(memory $MAIN_MEM))" > input.mem
+echo "(offset (value 3)(bank (join))(memory (main)))" > input.mem
 run
 
 echo "Success"
