aeb6f96108fec52ab7dc14856f48b74711fb7b4f
Joe Wingbermuehle <joewing@joewing.net>
2014-04-01 10:27:50 -0500

    Add command line parsing.

--- sim/main.ml
+++ sim/main.ml
@@ -1,12 +1,28 @@
+open Arg
 open Parse
 open Simulator
 
-let main () =
-    let directory = "/data/traces/" in
-    let (mach, main, mem, benchmarks) = parse_model_file "t" in
-    let sim = new simulator mach directory main mem benchmarks in
-    let t = sim#run in
-    print_int t;
-    print_newline ()
+(* Options. *)
+let directory = ref "/data/traces/";;
+let experiment = ref "";;
+
+(* Functions for parsing the command line. *)
+let set_directory d = directory := d;;
+let set_experiment e = experiment := e;;
+let opts = [
+    ("-d", Arg.String set_directory, "trace directory")
+];;
+
+let run_experiment () =
+    let (mach, main, mem, benchmarks) = parse_model_file !experiment in
+    let sim = new simulator mach !directory main mem benchmarks in
+    let time = sim#run in
+    print_int time; print_newline ()
 ;;
+
+let main () =
+    Arg.parse opts set_experiment "usage: sim [options] <experiment>";
+    run_experiment ()
+;;
+
 main ()
