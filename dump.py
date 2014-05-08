17e795faace2520090633bd9997507848572d85f
Joe Wingbermuehle <joewing@joewing.net>
2014-05-08 16:35:41 -0500

    Add -m option to dump.py.

--- dump.py
+++ dump.py
@@ -9,6 +9,8 @@ parser.add_option('-u', '--url', dest='url', default=None,
                   help='database URL')
 parser.add_option('-s', '--show', default=False, action='store_true',
                   help='show results')
+parser.add_option('-m', '--memory', default=False, action='store_true',
+                  help='dump memory spec')
 
 
 def print_print_ml(ml):
@@ -44,6 +46,18 @@ def show_state(db, experiments):
             print()
 
 
+def dump_spec(db, experiments):
+    names = get_name_map(db, experiments)
+    for mname, _, _ in db.get_status():
+        key = db.get_hash(mname)
+        if key in names:
+            best_name, _, _ = db.get_best(mname)
+            print(mname)
+            print('(memory')
+            print(best_name)
+            print(')')
+
+
 def show_pending(db, experiments):
     names = get_name_map(db, experiments)
     for mname, evals, value in db.get_status():
@@ -63,6 +77,8 @@ def main():
     db = database.get_instance(options.url)
     if options.show:
         show_state(db, args)
+    elif options.memory:
+        dump_spec(db, args)
     else:
         show_pending(db, args)
 
